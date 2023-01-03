06e1398d633e5c54901f737aa7be44f69671ae88
Đỗ Tuấn Đạt <datdt@dogoo.com.vn>
2023-01-03 10:14:18 +0700

    push code

--- src/components/progress-circle/index.js
+++ src/components/progress-circle/index.js
@@ -1,5 +1,23 @@
 import React from "react";
 
+const Circle = ({ percentage, color, size, strokeWidth }) => {
+	const radius = size / 2 - 10;
+	const circ = 2 * Math.PI * r - 20;
+	const strokePct = (100 - Math.round(percentage) * circ) / 100;
+
+	return (
+		<circle
+			r={radius}
+			cx="50%"
+			cy="50%"
+			fill="transparent"
+			stroke={strokePct !== circ ? color : ""}
+			strokeWidth={strokeWidth}
+			strokeDasharray
+		/>
+	);
+};
+
 const ProgressCircle = ({ percentage, isPlaying, size, color }) => {
 	return <div></div>;
 };
